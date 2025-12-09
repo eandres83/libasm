8cc5431273d0179836971395db9abd05d779c7fd
Eleder Andres <eandres@c3r1s5.42urduliz.com>
2025-12-09 13:39:52 +0100

    update main_strcpy.c, more tests

--- test/main_strcpy.c
+++ test/main_strcpy.c
@@ -13,6 +13,8 @@ int main()
 	char str2[12] = "Hola Dios?";
 	char dest2[4];
 
+	char *str3 = NULL;
+
 	ft_strcpy(dest, str);
 	write(1, dest, 4);
 
@@ -20,5 +22,14 @@ int main()
 
 	strcpy(dest2, str2);
 	write(1, dest2, 4);
+
+	ft_strcpy(dest, str3);
+	write(1, dest, 4);
+
+	write(1, "\n", 1);
+
+	strcpy(dest, str3);
+	write(1, dest, 4);
+
 	return (0);
 }
