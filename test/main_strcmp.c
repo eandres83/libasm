bde30063d3d61f6b211e83c66b5ca085e9d25d47
Eleder Andres <eandres@c3r1s5.42urduliz.com>
2025-12-09 13:39:22 +0100

    update main_strcmp.c, more tests

--- test/main_strcmp.c
+++ test/main_strcmp.c
@@ -7,9 +7,15 @@ int main()
 {
 	char s1[5] = "Hola";
 	char s2[5] = "Hila";
+	char *s3 = NULL;
+	char s4[] = "";
 
 	printf("result me: %d\n", ft_strcmp(s1, s2));
 	printf("result original: %d\n", strcmp(s1, s2));
+	printf("result me: %d\n", ft_strcmp(s1, s4));
+	printf("result original: %d\n", strcmp(s1, s4));
+	printf("result me: %d\n", ft_strcmp(s1, s3));
+	printf("result original: %d\n", strcmp(s1, s3));
 	return (0);
 }
 
