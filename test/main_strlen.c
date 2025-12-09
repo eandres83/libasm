8515bf91375cb9a0175bf36045dbbb28abb7934a
Eleder Andres <eandres@c3r1s5.42urduliz.com>
2025-12-09 13:40:09 +0100

    update main_strlen.c

--- test/main_strlen.c
+++ test/main_strlen.c
@@ -6,9 +6,18 @@ size_t	ft_strlen(char *str);
 int main()
 {
 	char *str = "Patatas";
+	char *str2 = NULL;
+	char *str3 = "";
+	char *str4 = ("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
 
 	printf("result me: %lu\n", ft_strlen(str));
 	printf("result original: %lu\n", strlen(str));
+	printf("result me: %lu\n", ft_strlen(str3));
+	printf("result original: %lu\n", strlen(str3));
+	printf("result me: %lu\n", ft_strlen(str4));
+	printf("result original: %lu\n", strlen(str4));
+	printf("result me: %lu\n", ft_strlen(str2));
+	printf("result original: %lu\n", strlen(str2));
 
 	return (0);
 }
